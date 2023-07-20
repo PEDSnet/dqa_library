9401ecb0d6c422deae1d1b740fe58efc910b9164
wieandk <wieandk@recover-chop-prd01.recover-covid19.com>
2023-07-20 13:33:52 +0000

    dynamic naming of precomp tables

--- code/precompute_tables.R
+++ code/precompute_tables.R
@@ -5,7 +5,7 @@ site_voml <- site_cdm_tbl('measurement_labs') %>%
     visit_occurrence_id, visit_concept_id)) %>%
   filter(visit_concept_id == 9202L)
 
-output_tbl(site_voml, 'site_voml',
+output_tbl(site_voml, paste0(config('site'), '_voml'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## Outpatient Med Admin
@@ -15,7 +15,7 @@ site_vodi <- site_cdm_tbl('drug_exposure') %>%
     visit_occurrence_id, visit_concept_id)) %>% 
   filter(visit_concept_id == 9202L)
 
-output_tbl(site_vodi, 'site_vodi', 
+output_tbl(site_vodi, paste0(config('site'), '_vodi'), 
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## Inpatient Prescriptions
@@ -25,7 +25,7 @@ site_vipdp <- site_cdm_tbl('drug_exposure') %>%
     visit_occurrence_id, visit_concept_id)) %>% 
   filter(visit_concept_id %in% c(9201L, 2000000048L))
 
-output_tbl(site_vipdp, 'site_vipdp',
+output_tbl(site_vipdp, paste0(config('site'), '_vipdp'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## Outpatient Procedures
@@ -34,7 +34,7 @@ site_prvo <- site_cdm_tbl('procedure_occurrence') %>%
     visit_occurrence_id, visit_concept_id)) %>% 
   filter(visit_concept_id == 9202L)
 
-output_tbl(site_prvo, 'site_prvo',
+output_tbl(site_prvo, paste0(config('site'), '_prvo'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## CKD Conditions
@@ -42,7 +42,7 @@ site_ckddx <- site_cdm_tbl('condition_occurrence') %>%
   inner_join(load_codeset('dx_ckd','iccccc'),
              by=c('condition_concept_id'='concept_id'))
 
-output_tbl(site_ckddx, 'site_ckddx',
+output_tbl(site_ckddx, paste0(config('site'), '_ckddx'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## HTN Prescription
@@ -51,6 +51,6 @@ site_htnrx <- site_cdm_tbl('drug_exposure') %>%
     load_codeset('rx_htn'),
     by=c('drug_concept_id'='concept_id'))
 
-output_tbl(site_htnrx, 'site_htnrx',
+output_tbl(site_htnrx, paste0(config('site'), '_htnrx'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
