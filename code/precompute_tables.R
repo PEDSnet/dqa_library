228e108a8a0b17115aeb3e9f0d7a17cc378845f2
wieandk <wieandk@chop.edu>
2024-11-21 15:40:24 -0500

    filter table to only have 1 date column

--- code/precompute_tables.R
+++ code/precompute_tables.R
@@ -12,17 +12,17 @@ output_tbl(site_voml, paste0(config('site'), '_voml'),
 site_vodi <- site_cdm_tbl('drug_exposure') %>%
   filter(drug_type_concept_id == 38000180L) %>%
   inner_join(select(site_cdm_tbl('visit_occurrence'),
-    visit_occurrence_id, visit_concept_id)) %>% 
+    visit_occurrence_id, visit_concept_id)) %>%
   filter(visit_concept_id == 9202L)
 
-output_tbl(site_vodi, paste0(config('site'), '_vodi'), 
+output_tbl(site_vodi, paste0(config('site'), '_vodi'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## Inpatient Prescriptions
 site_vipdp <- site_cdm_tbl('drug_exposure') %>%
   filter(drug_type_concept_id == 38000177L) %>%
   inner_join(select(site_cdm_tbl('visit_occurrence'),
-    visit_occurrence_id, visit_concept_id)) %>% 
+    visit_occurrence_id, visit_concept_id)) %>%
   filter(visit_concept_id %in% c(9201L, 2000000048L))
 
 output_tbl(site_vipdp, paste0(config('site'), '_vipdp'),
@@ -31,28 +31,30 @@ output_tbl(site_vipdp, paste0(config('site'), '_vipdp'),
 ## Outpatient Procedures
 site_prvo <- site_cdm_tbl('procedure_occurrence') %>%
   inner_join(select(site_cdm_tbl('visit_occurrence'),
-    visit_occurrence_id, visit_concept_id)) %>% 
+    visit_occurrence_id, visit_concept_id)) %>%
   filter(visit_concept_id == 9202L)
 
 output_tbl(site_prvo, paste0(config('site'), '_prvo'),
            indexes = c('person_id', 'visit_occurrence_id'))
 
 ## CKD Conditions
-site_ckddx <- site_cdm_tbl('condition_occurrence') %>% 
+site_ckddx <- site_cdm_tbl('condition_occurrence') %>%
   inner_join(load_codeset('dx_ckd','iccccc'),
-             by=c('condition_concept_id'='concept_id'))
+             by=c('condition_concept_id'='concept_id')) %>%
+  select(site, person_id, condition_concept_id, condition_start_date)
 
 output_tbl(site_ckddx, paste0(config('site'), '_ckddx'),
-           indexes = c('person_id', 'visit_occurrence_id'))
+           indexes = c('person_id'))
 
 ## HTN Prescription
-site_htnrx <- site_cdm_tbl('drug_exposure') %>% 
+site_htnrx <- site_cdm_tbl('drug_exposure') %>%
   inner_join(
     load_codeset('rx_htn'),
-    by=c('drug_concept_id'='concept_id'))
+    by=c('drug_concept_id'='concept_id')) %>%
+  select(site, person_id, drug_concept_id, drug_exposure_start_date)
 
 output_tbl(site_htnrx, paste0(config('site'), '_htnrx'),
-           indexes = c('person_id', 'visit_occurrence_id'))
+           indexes = c('person_id'))
 
 ## Inpatient > 2 days
 
